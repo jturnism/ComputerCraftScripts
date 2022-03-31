539cbfd1c69e1e564c0479ae99135fde90f5ffdf
Joseph Turner <jturnism@gmail.com>
2022-03-31 13:36:20 -0600

    fixed old variable name spots, and made battpercent global

--- reactorcontroll.lua
+++ reactorcontroll.lua
@@ -1,6 +1,9 @@
 --latest main code in raw : https://raw.githubusercontent.com/jturnism/ComputerCraftScripts/main/reactorcontroll.lua
 --connect modem to any computer side, connect modem to Activating reactor for power production
+
+--initalize global variables
 reactor = peripheral.wrap("BiggerReactors_Reactor_0")
+battpercent = 0
 
 --fuction to get reactor active status and return true or false depending on status
 function getactive()
@@ -17,8 +20,8 @@ function getbattpercent()
     local battobj = reactor.battery()
     local battcap = battobj.capacity()
     local battstoredcap = battobj.stored()
-    local battpercent = tonumber(math.floor((battstoredcap/battcap)*100))
-    return("Battery is at :", battpercent ,"%")
+    battpercent = tonumber(math.floor((battstoredcap/battcap)*100))
+    return("Battery is at : "..battpercent.."%")
 end
 
 --visuals
@@ -35,10 +38,10 @@ while true do
     print(getactive())
     print(getbattpercent())
     print("---------------------------------------------------")
-    if (intbattpercent<30) then
+    if (battpercent<30) then
         reactor.setActive(true)
         print("Activating reactor for power production")
-        while (intbattpercent<70) do
+        while (battpercent<70) do
             clear()
             print("---------------------------------------------------")
             print(getactive())
@@ -47,11 +50,11 @@ while true do
             print("Producing Power")
             sleep(1)
         end
-    elseif (intbattpercent>=30 and reactor.active()) then
+    elseif (battpercent>=30 and reactor.active()) then
         reactor.setActive(false)
         print("Deactivating reactor since sufficient battery capacity")
         print("---------------------------------------------------")
-    elseif (intbattpercent>=30 and not reactor.active()) then
+    elseif (battpercent>=30 and not reactor.active()) then
         reactor.setActive(false)
         print("Reactor stable and monitoring active")
         print("---------------------------------------------------")
