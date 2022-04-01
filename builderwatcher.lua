38242e9448b1524aa18f611738fbe166f1dd7de5
Joseph Turner <jturnism@gmail.com>
2022-03-31 18:15:15 -0600

    didn;t upload pseudo code last time

--- builderwatcher.lua
+++ builderwatcher.lua
@@ -1,1 +1,17 @@
---init
+--https://raw.githubusercontent.com/jturnism/ComputerCraftScripts/main/builderwatcher.lua
+
+--[[
+    pseudo code
+
+
+(most of the same as reactorcontrol.lua)
+
+if (builder.active) then
+    reactor.active(true) && override others
+elseif (not builder.active) then
+    reactor.active(false)
+else
+    print("Error or something IDK")
+end
+
+]]
