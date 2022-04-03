bcdd73591ae0cc6cb0dd218dc8ca623461583f37
Joseph Turner <jturnism@gmail.com>
2022-04-02 19:10:12 -0600

    test commit

--- reactorcontrol.lua
+++ reactorcontrol.lua
@@ -2,29 +2,42 @@
 --connect modem to any computer side, connect modem to reactor computer port, connect modem to monitor, connect all with network cable, rightclick each modem to activate
 
 --setup monitor for redirection (6monitors in 2x3 works best)
-term.redirect(peripheral.wrap("monitor_0"))
+--term.redirect(peripheral.wrap("monitor_0"))
 
 --initalize global variables
-reactor = peripheral.wrap("BiggerReactors_Reactor_0")
-battpercent = 0
 
 --fuction to get reactor active status and return true or false depending on status
-function getactive()
+function getreactoractive()
+    local reactor = peripheral.wrap("BiggerReactors_Reactor_0")
     local activestatus = reactor.active()
     if (activestatus) then
-        return("Reactor is active")
+        return(true)
     else
-        return("Reactor is inactive")
+        return(false)
     end 
 end
 
---function to get battery precent
-function getbattpercent()
+--function to print reactor status
+function printreactoractive()
+    if (getreactoractive()) then
+        print("Reactor is active")
+    else
+        print("Reactor is inactive")
+    end
+end
+
+--function to get reactor battery precent
+function getreactorbattpercent()
+    local reactor = peripheral.wrap("BiggerReactors_Reactor_0")
     local battobj = reactor.battery()
     local battcap = battobj.capacity()
     local battstoredcap = battobj.stored()
-    battpercent = tonumber(math.floor((battstoredcap/battcap)*100))
-    return("Battery is at : "..battpercent.."%")
+    local battpercent = math.floor((battstoredcap/battcap)*100)
+    return(battpercent)
+end
+
+function printreactorbattpercent()
+    print("Reactor battery is at : "..getreactorbattpercent().."%")
 end
 
 --visuals
@@ -34,38 +47,33 @@ function clear()
     term.setCursorPos(1,1)
 end
 
---main repeating code to monitor reactor
-while true do
+--visuals
+function line()
+    print("-----------------------------")
+end
+
+while true do --loop the main code
     clear()
-    print("-----------------------------")
-    print(getactive())
-    print(getbattpercent())
-    print("-----------------------------")
-    if (battpercent<30) then
+    line()
+    printreactoractive()
+    printreactorbattpercent()
+    if (getbattpercent()>=30) then -- if battery is over 30%, turn off nuclear reactor as its not really needed
+        reactor.setActive(false)
+        print("Deactivating reactor since sufficient battery capacity")
+    elseif (getbattpercent()<30) then -- if battery is less than 30% turn on nuclear reactor to fill up
         reactor.setActive(true)
         print("Activating reactor for power production")
-        while (battpercent<70) do
+        while (getbattpercent()<=70) do -- continue filling up until 70%
             clear()
-            print("-----------------------------")
-            print(getactive())
-            print(getbattpercent())
-            print("-----------------------------")
+            line()
+            printreactoractive()
+            printreactorbattpercent()
+            line()
             print("Producing Power")
             sleep(1)
         end
-    elseif (battpercent>=30 and reactor.active()) then
-        reactor.setActive(false)
-        print("Deactivating reactor since sufficient battery capacity")
-        print("-----------------------------")
-    elseif (battpercent>=30 and not reactor.active()) then
-        reactor.setActive(false)
-        print("Reactor stable and monitoring active")
-        print("-----------------------------")
     else
         print("Error")
-        print(getactive())
-        print(getbattpercent())
-        print("-----------------------------")
     end
     sleep(1)
 end
